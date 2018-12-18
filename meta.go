d6da261e9061ec1fad16b6748832203e9886c0d8
Ajith Hussain <csy0013@googlemail.com>
2018-12-18 22:49:07 +1100

    Moved meta to own file

---
+++ meta.go
@@ -0,0 +1,81 @@
+package helium
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/sparkymat/helium/charset"
+)
+
+// DeviceWidth represnts a viewport width of 'device-width'
+const DeviceWidth string = "device-width"
+
+func metaWithAttr(name string, content string) HeadNode {
+	node := HeadNode{name: "meta", hideClosingTag: true}
+	node.Attr("name", name)
+	node.Attr("content", content)
+	return node
+}
+
+// MetaCharset represents a <meta> tag for setting the charset
+func MetaCharset(value charset.Type) HeadNode {
+	node := HeadNode{name: "meta", hideClosingTag: true}
+	node.Attr("charset", value.String())
+	return node
+}
+
+// MetaAuthor represents a <meta> tag for setting the author
+func MetaAuthor(name string) HeadNode {
+	return metaWithAttr("author", name)
+}
+
+// MetaDescription represents a <meta> tag for setting the description
+func MetaDescription(name string) HeadNode {
+	return metaWithAttr("description", name)
+}
+
+// MetaGenerator represents a <meta> tag for setting the generator
+func MetaGenerator(name string) HeadNode {
+	return metaWithAttr("generator", name)
+}
+
+// MetaKeywords represents a <meta> tag for setting the keywords
+func MetaKeywords(keywords ...string) HeadNode {
+	return metaWithAttr("generator", strings.Join(keywords, " "))
+}
+
+// MetaViewport represents a <meta> tag for setting the viewport configuration
+func MetaViewport(width string, initialScale float64, minimumScale *float64, maximumScale *float64, userScalable *bool) HeadNode {
+	var values []string
+
+	values = append(values, fmt.Sprintf("width=%v", width))
+	values = append(values, fmt.Sprintf("initial-scale=%v", formatFloat(initialScale)))
+
+	if minimumScale != nil {
+		values = append(values, fmt.Sprintf("minimum-scale=%v", formatFloat(*minimumScale)))
+	}
+
+	if maximumScale != nil {
+		values = append(values, fmt.Sprintf("maximum-scale=%v", formatFloat(*maximumScale)))
+	}
+
+	if userScalable != nil {
+		if *userScalable == true {
+			values = append(values, "user-scalable=yes")
+		} else {
+			values = append(values, "user-scalable=no")
+		}
+	}
+
+	return metaWithAttr("viewport", strings.Join(values, ", "))
+}
+
+func formatFloat(value float64) string {
+	return strconv.FormatFloat(value, 'f', -1, 64)
+}
+
+// MetaViewportDefault returns a <meta> tag for viewport with device_width and scale of 1.0
+func MetaViewportDefault() HeadNode {
+	return MetaViewport(DeviceWidth, 1.0, nil, nil, nil)
+}
